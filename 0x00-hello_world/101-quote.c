425aa1da08ba096b1ec06c38128383b69c387f77
Cess-Moraa <cessmorah42@gmail.com>
2022-10-14 05:57:46 -0700

    Task 8 quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
+#include <studio.h>
 #include <unistd.h>
-
 /**
- *main - prints exactly and that piece of art is useful
+ * main - prints exactly and that piece of art is useful
  *
  * Return: Always 0 (Success)
  */
@@ -10,6 +9,6 @@ int main(void)
 
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	  return (1);
+	return (1);
 
 }
